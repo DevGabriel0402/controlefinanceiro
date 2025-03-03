bbd4a9a2302eb44abff247dc2b04e205fdc878f6
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-03-03 19:31:59 -0300

    atualizando path do main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { GlobalStyles } from "./styles/GlobalStyles";
+import { GlobalStyles } from "./styles/globalStyles/GlobalStyles";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
