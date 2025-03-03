06b72960141f21e0794b3f9ea64bc16ac26675a8
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-03-03 19:40:30 -0300

    Atualizando estilo do select

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -84,6 +84,7 @@ export const Select = styled.select`
   margin-bottom: 8px;
   padding: 5px;
   color: blueviolet;
+  background-color: #fafafa;
 
   @media (max-width: 550px) {
     max-width: 200px;
